5b78c59f0a26fae3db29390f42c883e6e6e2ec49
Erik Huang <ekhu94@gmail.com>
2021-05-04 07:15:29 -0700

    adds type annotation to log function

--- index.ts
+++ index.ts
@@ -3,21 +3,23 @@ import axios from 'axios';
 const URL = 'https://jsonplaceholder.typicode.com/todos/1';
 
 interface Todo {
-    id: number;
-    title: string;
-    completed: boolean;
+  id: number;
+  title: string;
+  completed: boolean;
 }
 
-axios.get(URL).then(res => {
-    const todo = res.data as Todo;
+axios.get(URL).then((res) => {
+  const todo = res.data as Todo;
 
-    const id = todo.id;
-    const title = todo.title;
-    const completed = todo.completed;
+  const { id, title, completed } = todo;
 
-    console.log(`
+  logTodo(id, title, completed);
+});
+
+const logTodo = (id: number, title: string, completed: boolean) => {
+  console.log(`
         Todo with ID: ${id}
         Title: ${title}
         Is Finished: ${completed}
-    `)
-});
+    `);
+};
