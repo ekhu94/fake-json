cef35b06ce966980e3d0686a3f93d5f719d6ae5c
Erik Huang <ekhu94@gmail.com>
2021-05-04 07:05:25 -0700

    declares interface for Todo to catch property errors

--- index.ts
+++ index.ts
@@ -2,4 +2,22 @@ import axios from 'axios';
 
 const URL = 'https://jsonplaceholder.typicode.com/todos/1';
 
-axios.get(URL).then(res => console.log(res.data));
+interface Todo {
+    id: number;
+    title: string;
+    completed: boolean;
+}
+
+axios.get(URL).then(res => {
+    const todo = res.data as Todo;
+
+    const id = todo.id;
+    const title = todo.title;
+    const completed = todo.completed;
+
+    console.log(`
+        Todo with ID: ${id}
+        Title: ${title}
+        Is Finished: ${completed}
+    `)
+});
